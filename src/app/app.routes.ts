c0b41d10615048da4d84327854c55a35ba26f68f
Happid <happidilmi@gmail.com>
2025-12-15 16:13:29 +0700

    push all routes angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { AuthGuard } from './core/auth-guard';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: 'home',
+    // Lazy Loading Component
+    loadComponent: () => import('./pages/home/home').then(c => c.Home),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'profile',
+    // Lazy Loading Component
+    loadComponent: () => import('./pages/profile/profile').then(c => c.Profile),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'profile/:id',
+    // Lazy Loading Component
+    loadComponent: () => import('./pages/profile/detail/detail').then(c => c.Detail),
+    canActivate: [AuthGuard]
+  },
+  {
+    path: 'not-found',
+    // Lazy Loading Component
+    loadComponent: () => import('./pages/not-found/not-found').then(c => c.NotFound),
+  },
+  { path: '**', redirectTo: 'not-found' }
+];
