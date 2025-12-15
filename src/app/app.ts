2da5c58898a15628f04f291295ab4e2fcb119d4a
Happid <happidilmi@gmail.com>
2025-12-15 14:10:29 +0700

    dependency injection, injection and injector

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './services/api-service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,8 @@ import { RouterOutlet } from '@angular/router';
 export class App {
   protected readonly title = signal('pre-interview-angular');
   x = 's';
+
+  constructor(
+    private service: ApiService // INI INJECTION
+  ) {}
 }
---
+++ src/app/services/api-service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({ // INI INJECTOR
+  providedIn: 'root', // 'root' berarti GLOBAL
+})
+export class ApiService {
+
+}
